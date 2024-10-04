31cbd4fa1c4ba963226a87d3e94b5a8f87ff3fda
CedtT198 <cedrictiavina426@gmail.com>
2024-10-04 18:01:20 -0500

    all config init done

--- schema.sql
+++ schema.sql
@@ -2,17 +2,15 @@ DROP DATABASE pain;
 CREATE DATABASE pain;
 USE pain;
 
--- creation type de charge_repartition
 CREATE TABLE type_charge(
     id_type_charge INT PRIMARY KEY auto_increment,
     nom_type_charge VARCHAR(255) not null
 );
 
--- creatio table nature(fixe ou variable)
 CREATE TABLE nature(
     id_nature INT PRIMARY KEY auto_increment,
     nom_nature VARCHAR(255) not null
-),
+);
 
 CREATE TABLE unite_oeuvre(
     id_unite_oeuvre INT PRIMARY KEY auto_increment,
@@ -42,19 +40,19 @@ ALTER TABLE charge ADD FOREIGN KEY (id_rubrique) REFERENCES rubrique(id_rubrique
 ALTER TABLE charge ADD FOREIGN KEY (id_type) REFERENCES type_charge(id_type_charge);
 
 
-CREATE TABLE repartition(
-    id_repartition INT PRIMARY KEY auto_increment,
-    nom_repartition VARCHAR(255) not null
+CREATE TABLE centre(
+    id_centre INT PRIMARY KEY auto_increment,
+    nom_centre VARCHAR(255) not null
 );
 
-CREATE TABLE charge_repartition(
-    id_charge_repartition INT PRIMARY KEY auto_increment,
+CREATE TABLE repartition_centre(
+    repartition_centre INT PRIMARY KEY auto_increment,
     id_charge INT not null,
-    id_repartition INT not null,
+    id_centre INT not null,
     taux DECIMAL(15,3)
 );
-ALTER TABLE charge_repartition ADD FOREIGN KEY (id_charge) REFERENCES charge(id_charge); 
-ALTER TABLE charge_repartition ADD FOREIGN KEY (id_repartition) REFERENCES repartition(id_repartition); 
+ALTER TABLE repartition_centre ADD FOREIGN KEY (id_charge) REFERENCES charge(id_charge); 
+ALTER TABLE repartition_centre ADD FOREIGN KEY (id_centre) REFERENCES centre(id_centre); 
 
 
 INSERT INTO type_charge (id_type_charge,nom_type_charge) VALUES 
@@ -69,7 +67,7 @@ INSERT INTO nature (id_nature,nom_nature) VALUES
 INSERT INTO unite_oeuvre (nom_unite_oeuvre, abreviation) VALUES
 ("Kilogramme", "Kg");
 
-INSERT INTO repartition VALUES
+INSERT INTO centre VALUES
 (null, "Courses"),
 (null, "Usine"),
 (null, "Administration"),
