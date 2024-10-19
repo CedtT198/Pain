236b1a76b761cbf014e6fcefb0a156fe90a0e0e7
TsiryNat <nantenainatsiry9@gmail.com>
2024-10-19 21:30:30 +0300

    fournisseur

---
+++ Fournisseur/application/views/template/footer.php
@@ -0,0 +1,51 @@
+          <!-- ================== fin ampovoany  ================= -->
+          <!-- ================== fin ampovoany  ================= -->
+          <!-- ================== fin ampovoany  ================= -->
+          </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+          </div>
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
+          </div>
+        </footer> 
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>   
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:assets/js -->
+  <script src="<?php echo base_url(); ?>assets/vendors/js/vendor.bundle.base.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendors/assets/js/vendor.bundle.base.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendors/chart.assets/js/Chart.min.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendors/datatables.net/jquery.dataTables.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/dataTables.select.min.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/off-canvas.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/template.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/settings.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/todolist.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/dashboard.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/Chart.roundedBarCharts.assets/js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/template.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/settings.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/todolist.js"></script>
+  <!-- <script src="<?php echo base_url(); ?>assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendors/select2/select2.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/file-upload.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/typeahead.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/select2.js"></script> -->
+
+</body>
+
+</html>
