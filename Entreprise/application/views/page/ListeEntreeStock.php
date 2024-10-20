462b28b3537542ae93219b5ac749a7a6df4029f0
TsiryNat <nantenainatsiry9@gmail.com>
2024-10-20 10:59:17 +0300

    entree stock

---
+++ Entreprise/application/views/page/ListeEntreeStock.php
@@ -0,0 +1,36 @@
+<div class="row">
+       <div class="col-md-3"></div>
+       <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                     <div class="card-body">
+                            <h4 class="card-title">Entree stock</h4>
+                            <p class="card-description">formulaire</p>
+                            <form class="forms-sample">
+                                   <div class="form-group row">
+                                          <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Date</label>
+                                          <div class="col-sm-9">
+                                                 <input type="Date" class="form-control" id="exampleInputUsername2" placeholder="Date">
+                                          </div>
+                                   </div>
+                                   <div class="form-group row">
+                                          <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Quantite</label>
+                                          <div class="col-sm-9">
+                                                 <input type="number" class="form-control" id="exampleInputEmail2" placeholder="00">
+                                          </div>
+                                   </div>
+                                   <div class="form-group row">
+                                          <label class="col-sm-3 col-form-label">Produit</label>
+                                          <div class="col-sm-9">
+                                                 <select class="form-control">
+                                                 <option>Produit 1</option>
+                                                 <option>Produit 2</option>
+                                                 </select>
+                                          </div>
+                                   </div>
+                                   <button type="button" class="btn btn-primary btn-lg btn-block">Submit</button>
+                                   <!-- <button type="submit" class="btn btn-primary mr-2">Submit</button> -->
+                            </form>
+                     </div>
+              </div>
+       </div>
+</div>
