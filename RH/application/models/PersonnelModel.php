0a4d6817f5695f4525629524858c9f066a78ce69
Bedouu <155775889+Bedouu@users.noreply.github.com>
2024-11-10 01:46:05 +0300

    blabla

--- RH/application/models/PersonnelModel.php
+++ RH/application/models/PersonnelModel.php
@@ -1,4 +1,21 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 class PersonnelModel extends CI_Model {
+    public function insert($nom, $prenom, $dateNaissance, $idPoste)
+    {
+        $data = array(
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'date_naissance' => $dateNaissance,
+            'id_poste' => $idPoste
+        );
+
+        return $this->db->insert('personnel', $data);
+    }
+    public function getById($id){
+        $this->db->where('id_personnel', $id);
+        $query=$this->db->get('personnel');
+        return $query->result();
+    }
 }
 ?>
