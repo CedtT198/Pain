9942c752ad258f79bf4542a986b3a4db2d721069
TsiryNat <nantenainatsiry9@gmail.com>
2024-11-10 08:16:56 +0300

    front

--- RH/application/views/page/FormulaireCandidature.php
+++ RH/application/views/page/FormulaireCandidature.php
@@ -6,7 +6,7 @@
                             <div class="row">
                                    <div class="col-md-3"></div>
                                    <div class="col-md-8">
-                                          <h4 class="card-title">FORMULAIRE D'INSERTION CANDIDATURE</h4>
+                                          <h4 class="card-title">FORMULAIRE D'INSERTION CANDIDATURE </h4>
                                    </div>
                             </div>
                             <p class="card-description">Message :  <span class="text-info"> Candidature Enregistre ! </span> </p>
